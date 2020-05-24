b5d813dfae60eb058008da9ef3f8fe6494416883
Anna Dontcova <a.dontcova@gmail.com>
2020-05-24 20:03:59 +0200

    moves Enzyme setup into setupTest.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,10 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
-import EnzymeAdapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
 
 import App from "./App";
 import { findByTestAttr } from "../test/testUtils";
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
-
-const setup = (props = {}, state = null) => {
+const setup = (props = {}, state = false) => {
   const wrapper = shallow(<App {...props} />);
   if (state) wrapper.setState(state);
   return wrapper;
@@ -17,4 +14,10 @@ test("renders without error", () => {
   const wrapper = setup();
   const appComponent = findByTestAttr(wrapper, "component-app");
   expect(appComponent.length).toBe(1);
+});
+
+test("initial 'success' state is false", () => {
+  const wrapper = setup();
+  const initialState = wrapper.state("success");
+  expect(initialState).toBe(false);
 });
